1bd5558d1a3045c467612dce6877450d65c43cbf
Atul Manjhi <163382635+atulmanjhi1@users.noreply.github.com>
2024-04-30 16:33:14 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from groq import Groq
+
+# Initialize the Groq client
+client = Groq(
+    api_key="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",
+)
+
+def generate_chat_completion(system_message, user_message):
+    # Generate chat completion
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": system_message,
+            },
+            {
+                "role": "user",
+                "content": user_message,
+            }
+        ],
+        model="llama3-70b-8192",
+    )
+    
+    return chat_completion.choices[0].message.content
+
+# Streamlit app layout
+st.title("AMJ's Chat Bot ")
+
+# Input fields for system and user messages
+system_message = st.text_input("System Message:")
+user_message = st.text_input("User Message:")
+
+# Submit button to generate chat completion
+if st.button("Generate Response"):
+    # Check if both inputs are not empty
+    if system_message and user_message:
+        # Generate chat completion and display it
+        response = generate_chat_completion(system_message, user_message)
+        st.write(f"Response: {response}")
+    else:
+        st.warning("Please fill in both fields.")
